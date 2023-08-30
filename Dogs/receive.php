cd016e5245442d2ee9b8914927fd97870036dbd4
yPauloR <132622557+yPauloR@users.noreply.github.com>
2023-08-29 23:28:52 -0300

    Update receive.php

--- Dogs/receive.php
+++ Dogs/receive.php
@@ -1,5 +1,7 @@
 <?php
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
 
 $name = $_POST['name'];
 $selectedBreedName = $_POST['breed'];
